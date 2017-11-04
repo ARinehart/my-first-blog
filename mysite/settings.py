56664aa2eb50d7829d7487566ec5ba5a1abdf7c6
aliciarinehart <aliciarinehart@hotmail.com>
2017-11-04 14:34:15 -0700

    fixed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2!sm)*705w-poc-s+=!y9rpcuo7^e47*)*38gjzmzfp9%-dk39'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<AliciaRinehart.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'AliciaRinehart.pythonanywhere.com']
 
 
 # Application definition
